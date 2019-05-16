76687e96ea011e4a80f4b8b04b2fedb2d157f4c6
Mila Roisin <mila.roisin@gmail.com>
2019-05-16 15:27:07 -0400

    Updated pandasframe for github CSV file reading; added second graph.

--- app.py
+++ app.py
@@ -7,6 +7,10 @@ import pandas as pd
 
 ##data: MacOSX - 9, Windows - 4, Debian - 2, Other - 0
 
+url = 'https://raw.githubusercontent.com/milaroisin/plotlyDashOS/master/dataset/plotlyoperatingsystems.csv'
+
+os_data = pd.read_csv(url)
+
 app = dash.Dash()
 app.title = 'Dash App: Operating Systems - Interns'
 
@@ -14,28 +18,57 @@ app.title = 'Dash App: Operating Systems - Interns'
 app.css.append_css({'external_url': 'https://bootswatch.com/4/lux/bootstrap.css'})
 
 app.layout = html.Div(
+    html.Div([
+        dcc.Graph(
+            id='barchart',
+            figure={
+                'data': [
+                    {'x': ['MacOSX', 'Windows', 'Linux', 'Other'],
+                     'y': [9, 4, 2, 0], 'type': 'bar'}],
+                'layout': {
+                    'title': 'Plot.ly: Summer 2019 Preferred Operating Systems',
+                    'xaxis': dict(
+                        title='Operating Systems',
+                        titlefont=dict(
+                        family='Old Standard TT',
+                        size=20,
+                        color='#7f7f7f'
+                    )),
+                    'yaxis' : dict(
+                        title='# of Interns',
+                        titlefont=dict(
+                        family='Old Standard TT',
+                        size=20,
+                    ))
+                }
+            }
+        ),
 
-    dcc.Graph(
+        dcc.Graph(
+            id='example-graph-2',
+            figure={
+                'data': [
+                    {'x': ['MacOSX', 'Windows', 'Linux', 'Other'],
+                     'y': [9, 4, 2, 0], 'type': 'line'}],
+                'layout': {
+                    'title': 'Plot.ly: Summer 2019 Preferred Operating Systems',
+                    'xaxis': dict(
+                        title='Operating Systems',
+                        titlefont=dict(
+                        family='Old Standard TT',
+                        size=20
+                        )),
+                    'yaxis': dict(
+                    title='# of Interns',
+                    titlefont=dict(
+                    family='Old Standard TT',
+                        size=20
+                    ))
+                }
+            }
+        )
 
-        # barchart
-        id='barchart',
-              figure={
-                  'data': [{'x': ['MacOSX', 'Windows', 'Linux', 'Other'], 'y': [9, 4, 2, 0], 'type': 'bar'}],
-                  'layout': {
-                      'title': 'Plot.ly: Summer 2019 Preferred Operating Systems',
-                      'xaxis': dict(
-                          title='Operating Systems',
-                          titlefont=dict(
-                              family='Old Standard TT',
-                              size=20)),
-                      'yaxis': dict(
-                          title='# of Interns',
-                          titlefont=dict(
-                              family='Old Standard TT',
-                              size=20))
-                  }
-              }
-              )
+    ])
 )
 
 
