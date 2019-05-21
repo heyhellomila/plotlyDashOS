3a23980766b522438c039200e5eadbc1298456f0
Mila Roisin <mila.roisin@gmail.com>
2019-05-20 20:52:49 -0400

    Donut chart updated with dataset

--- app.py
+++ app.py
@@ -4,42 +4,43 @@ import dash_html_components as html
 import dash_core_components as dcc
 import pandas as pd
 
+url = 'https://raw.githubusercontent.com/milaroisin/plotlyDashOS/master/dataset/plotlyoperatingsystems.csv'
+os_data = pd.read_csv(url)
 
 ##data: MacOSX - 9, Windows - 4, Debian - 2, Other - 0
 
 app = dash.Dash()
 app.title = 'Dash App: Operating Systems - Interns'
 
-## css elements with bootstrap
-app.css.append_css({'external_url': 'https://bootswatch.com/4/lux/bootstrap.css'})
+## css elements for grid alignment
 
-app.layout = html.Div(
+app.css.append_css({'external_url': 'https://codepen.io/amyoshino/pen/jzXypZ.css'})
 
-    dcc.Graph(
+app.layout = html.Div([
 
-        # barchart
-        id='barchart',
-              figure={
-                  'data': [{'x': ['MacOSX', 'Windows', 'Linux', 'Other'], 'y': [9, 4, 2, 0], 'type': 'bar'}],
-                  'layout': {
-                      'title': 'Plot.ly: Summer 2019 Preferred Operating Systems',
-                      'xaxis': dict(
-                          title='Operating Systems',
-                          titlefont=dict(
-                              family='Old Standard TT',
-                              size=20)),
-                      'yaxis': dict(
-                          title='# of Interns',
-                          titlefont=dict(
-                              family='Old Standard TT',
-                              size=20))
-                  }
-              }
-              )
-)
-
-
-# piechart with hole
+##Donut Chart
+dcc.Graph(
+    id='piechart',
+    figure={
+        'data': [{'labels': ['MacOSX', 'Windows', 'Linux', 'Other'],
+                  'values': [60, 26.7, 13., 0],
+                  'type': 'pie',
+                  'hole': 0.5}],
+        'layout': {
+            'title': 'Plot.ly: Summer 2019 Preferred Operating Systems',
+            'xaxis': dict(
+                title='Operating Systems',
+                titlefont=dict(
+                    family='Old Standard TT',
+                    size=20)),
+            'yaxis': dict(
+                title='# of Interns',
+                titlefont=dict(
+                    family='Old Standard TT',
+                    size=20))
+        }
+    }
+)])
 
 
 ## return event statement
